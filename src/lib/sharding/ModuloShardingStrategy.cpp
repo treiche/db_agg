6a99b7ef07ca9bb27ecf91603c3f99370b8b07cc
Arnd Brusdeilins <arnd.brusdeilins@zalando.de>
2014-10-19 18:06:07 +0200

    added modulo sharding strategy

---
+++ src/lib/sharding/ModuloShardingStrategy.cpp
@@ -0,0 +1,35 @@
+/*
+ * ModuloShardingStrategy.cpp
+ *
+ *  Created on: Mar 30, 2014
+ *      Author: arnd
+ */
+
+#include "ModuloShardingStrategy.h"
+
+#include "utils/logging.h"
+#include "utils/md5.h"
+
+using namespace std;
+using namespace log4cplus;
+
+namespace db_agg {
+static Logger LOG = Logger::getInstance(LOG4CPLUS_TEXT("ModuloShardingStrategy"));
+
+int ModuloShardingStrategy::getShardId(std::string shardKey) {
+	int iKey = stoi(shardKey);
+	cout << "shardCount = " << shardCount << endl;
+	cout << "ikey = " << iKey << " = " << ((iKey % shardCount) + 1) << endl;
+    return iKey % shardCount + 1;
+}
+
+void ModuloShardingStrategy::setShardCount(int shardCount) {
+    this->shardCount = shardCount;
+}
+
+}
+
+
+
+
+
