439fcd453999ce746598b60416a791486af8a16c
Arnd Brusdeilins <arnd.brusdeilins@zalando.de>
2014-12-15 15:00:06 +0100

    switch to google mock

--- src/bin/testrunner.cpp
+++ src/bin/testrunner.cpp
@@ -1,51 +1,19 @@
-/* Generated file, do not edit */
+/*
+ * Main.cpp
+ *
+ *  Created on: Oct 24, 2014
+ *      Author: arnd
+ */
 
-#ifndef CXXTEST_RUNNING
-#define CXXTEST_RUNNING
-#endif
-
-#define _CXXTEST_HAVE_STD
-#define _CXXTEST_HAVE_EH
-#include <cxxtest/TestListener.h>
-#include <cxxtest/TestTracker.h>
-#include <cxxtest/TestRunner.h>
-#include <cxxtest/RealDescriptions.h>
-#include <cxxtest/TestMain.h>
-#include <cxxtest/ErrorPrinter.h>
-#include <cxxtest/XmlPrinter.h>
-#include <cxxtest/Descriptions.h>
-#include "utils/logging.h"
-#include <log4cplus/consoleappender.h>
+#include "gtest/gtest.h"
+#include <log4cplus/logger.h>
 #include <log4cplus/fileappender.h>
 #include <log4cplus/layout.h>
-#include <log4cplus/ndc.h>
-#include <log4cplus/helpers/loglog.h>
-#include <log4cplus/helpers/threads.h>
-#include <log4cplus/helpers/sleep.h>
 
-#include <log4cplus/configurator.h>
-#include <iostream>
-#include <string>
 
-using namespace std;
+using ::testing::InitGoogleTest;
 using namespace log4cplus;
 using namespace log4cplus::helpers;
-
-class TestListenerImpl : public CxxTest::ErrorPrinter {
-private:
-    Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("TestListenerImpl"));
-public:
-    virtual void enterTest(const CxxTest::TestDescription & desc) override {
-        LOG4CPLUS_DEBUG(logger, "enterTest " << desc.testName());
-        // CxxTest::ErrorPrinter::enterTest(desc);
-    }
-    virtual void enterWorld(const CxxTest::WorldDescription & desc) override {
-        std::cout << "enterWorld " << desc.worldName() << std::endl;
-    }
-    virtual void enterSuite(const CxxTest::SuiteDescription & desc) override {
-        LOG4CPLUS_DEBUG(logger, "enterSuite " << desc.suiteName());
-    }
-};
 
 void initLogging() {
     // PropertyConfigurator::doConfigure("src/test/resources/log4cplus.properties");
@@ -58,21 +26,12 @@ void initLogging() {
     Logger::getRoot().setLogLevel(DEBUG_LOG_LEVEL);
 }
 
-int main( int argc, char *argv[] ) {
-    initLogging();
-    Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("main"));
-    LOG4CPLUS_DEBUG(logger, "run testrunner ");
-    for (int idx=0;idx<argc;idx++) {
-        LOG4CPLUS_INFO(logger, "arg " << idx << " = " << argv[idx]);
-    }
-    LOG4CPLUS_DEBUG(logger, "start running tests ...");
 
-    int status;
-    CxxTest::ErrorPrinter tmp;
-    TestListenerImpl listener;
-    CxxTest::setAbortTestOnFail(true);
-    status = CxxTest::Main<TestListenerImpl>( listener, argc, argv );
-    return status;
+int main(int argc, char **argv) {
+	initLogging();
+	InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
-#include <cxxtest/Root.cpp>
-const char* CxxTest::RealWorldDescription::_worldName = "cxxtest";
+
+
+
