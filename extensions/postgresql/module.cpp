b79f49b9d8e6bf38881995d1280c0508913fe0ae
Arnd Brusdeilins <arnd.brusdeilins@zalando.de>
2014-12-16 17:24:00 +0100

    postgresql as external extension

---
+++ extensions/postgresql/module.cpp
@@ -0,0 +1,57 @@
+/*
+ * testmodel.cpp
+ *
+ *  Created on: Dec 21, 2013
+ *      Author: arnd
+ */
+#include <iostream>
+#include <memory>
+#include "db_agg.h"
+#include "PGQueryExecution.h"
+
+#include <log4cplus/logger.h>
+
+
+using namespace std;
+using namespace log4cplus;
+using namespace db_agg;
+
+DECLARE_LOGGER("PostgresqlExtension");
+
+class PostgresqlExtension : public Extension {
+private:
+    map<ComponentType,vector<string>> components{
+        {ComponentType::QUERY_EXECUTION, { "postgresql"}}
+    };
+public:
+    virtual ~PostgresqlExtension() override;
+    virtual shared_ptr<ShardingStrategy> getShardingStrategy(string name) override;
+    virtual QueryExecution *getQueryExecution(std::string name) override;
+    virtual map<ComponentType,vector<string>> getProvidedComponents() override;
+};
+
+map<ComponentType,vector<string>> PostgresqlExtension::getProvidedComponents() {
+    return components;
+}
+
+QueryExecution *PostgresqlExtension::getQueryExecution(std::string name) {
+    if (name == "postgresql") {
+        return new db_agg::PGQueryExecution();
+    }
+    return nullptr;
+}
+
+shared_ptr<db_agg::ShardingStrategy> PostgresqlExtension::getShardingStrategy(string name) {
+    LOG_DEBUG("called getShardingStrategy(" << name << ")" );
+    return nullptr;
+}
+
+PostgresqlExtension::~PostgresqlExtension() {
+    LOG_DEBUG("called destructor");
+}
+
+extern "C" {
+db_agg::Extension *getExtension() {
+    return new PostgresqlExtension();
+}
+}
