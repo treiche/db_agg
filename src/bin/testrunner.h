64e940fe31509b597a840393193a9a8584a8603f
Arnd Brusdeilins <arnd.brusdeilins@zalando.de>
2014-12-16 16:33:20 +0100

    start testing based on sqlite3 and url registry

---
+++ src/bin/testrunner.h
@@ -0,0 +1,18 @@
+/*
+ * testrunner.h
+ *
+ *  Created on: Dec 15, 2014
+ *      Author: arnd
+ */
+
+#ifndef TESTRUNNER_H_
+#define TESTRUNNER_H_
+
+
+#include <string>
+
+extern std::string TEST_DATA_DIR;
+
+
+
+#endif /* TESTRUNNER_H_ */
